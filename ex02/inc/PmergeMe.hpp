ffba2c20cf3fccbb170367be984610afc26fd5fc
hramaros <hramaros@student.42antananarivo.mg>
2025-04-10 09:18:21 +0300

    effectue:     - prototypage de la classe OK TODO:     - definition des methodes de la classe

--- ex02/inc/PmergeMe.hpp
+++ ex02/inc/PmergeMe.hpp
@@ -3,6 +3,35 @@
 #include <iostream>
 #include <cstdlib>
 #include <sstream>
-#include <stack>
-#include <algorithm>
 #include <string>
+
+#include <deque>
+#include <vector>
+
+class PmergeMe
+{
+    public:
+        PmergeMe();
+        PmergeMe(PmergeMe &src);
+        PmergeMe&operator=(PmergeMe &src);
+        ~PmergeMe();
+
+        void insertDeque(unsigned int value);
+        void insertVector(unsigned int value);
+
+        void sortDeque(void);
+        void sortVector(void);
+
+        void dequeSpitter(void);
+        void vectorSpitter(void);
+
+        void displayDequeTime(void);
+        void displayVectorTime(void);
+
+    private:
+        std::deque<unsigned int> dque;
+        std::vector<unsigned int> vect;
+
+        float dqueTime;
+        float vectTime;
+};
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,8 +1,36 @@
 #include "PmergeMe.hpp"
 
+int valid_args(std::string arg)
+{
+    for (size_t i = 0; i < arg.length(); i++)
+        if (!isdigit(arg[i]))
+            return (0);
+    return (1);
+}
+
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
+    PmergeMe merger;
+
+    if (argc < 2 || !valid_args(argv[1]))
+    {
+        std::cout << "\e[31mError: argument invalid\e[0m" << std::endl;
+        return (1);
+    }
+    for (size_t i = 0; argv[i]; i++)
+    {
+        merger.insertVector(atoi(argv[i]));
+        merger.insertDeque(atoi(argv[i]));
+    }
+    std::cout << "Before:   "; merger.dequeSpitter(); std::cout << std::endl;
+
+    merger.sortDeque();
+    merger.sortVector();
+
+    std::cout << "After:    "; merger.dequeSpitter(); std::cout << std::endl;
+
+    merger.displayDequeTime();
+    merger.displayDequeTime();
+
     return (0);
 }
